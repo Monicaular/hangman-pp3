44c998190ae50b40f34cd6a4dcd6eedd2c54282d
Monicaular <>
2023-12-30 12:28:25 +0000

    Add function for displaying the games rules

--- run.py
+++ run.py
@@ -16,3 +16,38 @@ def show_title():
 Welcome to Hangman Game!
     """
     print(game_title)
+
+def show_rules():
+    """
+    Displays the game's rules - if the users wishes to
+    """
+    display_rules = input("Would you like to see the game rules? (Y/N)\n ")
+    while display_rules.upper() not in ["Y", "N"]:
+        display_rules = input('Please only insert Y or N\n').upper()
+    
+    if display_rules.upper() == "Y":
+        rules = [
+        "Insert your username so we know who are we playing with",
+        "Choose a difficulty level between easy, medium, and hard",
+        "Choose a letter or a word to guess",
+        "A correct letter will show you its position(s) in the word",
+        "A wrong letter would add to the hangman",
+        "You have six tries to guess, otherwise the man will be hanged",
+        "Let's begin the fun"
+        ]
+        print("\nPlaying Rules:\n ")
+        # displays the rules in multiple lines
+        for rule in rules:
+            print(rule)
+    else:
+        print("You choose to skip the rules. Let's start the game")
+
+def main():
+    show_title()
+    show_rules()
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
