944d4bc7cc27bf13e60417629724ee3e128e8326
Monicaular <>
2023-12-30 12:25:25 +0000

    Add function for displaying the title

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def show_title():
+    """
+    Shows the the name of the game in a more graphical style
+    """
+    
+    game_title = """
+  _    _            _   _   _____  __  __            _   _ 
+ | |  | |    /\    | \ | | / ____||  \/  |    /\    | \ | |
+ | |__| |   /  \   |  \| || |  __ | \  / |   /  \   |  \| |
+ |  __  |  / /\ \  | . ` || | |_ || |\/| |  / /\ \  | . ` |
+ | |  | | / ____ \ | |\  || |__| || |  | | / ____ \ | |\  |
+ |_|  |_|/_/    \_\|_| \_| \_____||_|  |_|/_/    \_\|_| \_|
+                                                         
+Welcome to Hangman Game!
+    """
+    print(game_title)
