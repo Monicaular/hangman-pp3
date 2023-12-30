e70c84749060aa2fe4ae6108680af2174182d8eb
Monicaular <>
2023-12-30 12:26:26 +0000

    Add words.py file to the repository

---
+++ words.py
@@ -0,0 +1,115 @@
+easy_level_words = [
+    'lion',
+    'italy',
+    'apple',
+    'pizza',
+    'kiwi',
+    'dog',
+    'bird',
+    'star',
+    'pen',
+    'key',
+    'hat',
+    'smile',
+    'frog',
+    'jump',
+    'book',
+    'ball',
+    'tree'
+]
+
+medium_level_words = [
+    'puzzle',
+    'galaxy',
+    'caramel',
+    'goggles',
+    'venture',
+    'journey',
+    'explorer',
+    'twilight',
+    'treasure',
+    'captain',
+    'iceberg',
+    'rainbow',
+    'thunder',
+    'bicycle',
+    'pancakes',
+    'baseball',
+    'lasagna', 
+    'popcorn'
+]
+
+hard_level_words = [
+'clandestine',
+'effervescent',
+'juxtaposition',
+'perspicacious',
+'serendipity',
+'cacophony',
+'idiosyncrasy',
+'quintessential',
+'esoteric',
+'ubiquitous',
+'mellifluous',
+'phantasmagoria',
+'sesquipedalian',
+'discombobulate',
+'unscrupulous',
+'ephemeral',
+'superfluous',
+'antediluvian',
+'unequivocal',
+'prevaricate',
+]
+
+hangman_graphic = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========='''
+]
